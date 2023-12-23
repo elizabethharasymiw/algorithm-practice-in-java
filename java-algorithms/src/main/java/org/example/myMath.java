bc0a3d2e2f18af87e9362b089b4d27d1c6097170
Elizabeth Harasymiw <Elizabeth.Harasymiw@gmail.com>
2023-12-22 23:09:58 -0600

    Add interative solution to factorial

--- java-algorithms/src/main/java/org/example/myMath.java
+++ java-algorithms/src/main/java/org/example/myMath.java
@@ -3,4 +3,21 @@ package org.example;
 public class myMath {
     private myMath(){
     }
+
+    /*
+     * Factorial, n!
+     * 1! = 1 = 1
+     * 2! = 1 * 2 = 2
+     * 3! = 1 * 2 * 3 = 6
+     * 4! = 1 * 2 * 3 * 4 = 24
+     */
+
+    // Interative Solution
+    static int factorialI(int n){
+        int result = n;
+        for(int i = 1; i < n; i++) {
+            result = result * i;
+        }
+        return result;
+    }
 }
