108781258927828376a2dae67ac8189f1183b347
Elizabeth Harasymiw <elizabethharasymiw@Elizabeths-MacBook-Pro.local>
2024-01-16 14:20:43 -0600

    Add new empty sort class with todo list items

---
+++ java-algorithms/src/main/java/org/example/mySort.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class mySort {
+    private mySort(){
+    }
+
+    //TODO Selection Sort
+    //TODO Bubble Sort
+    //TODO Insertion Sort
+    //TODO Merge Sort
+    //TODO Quick Sort
+    //TODO Heap Sort
+    //TODO Counting Sort
+    //TODO Radix Sort
+    //TODO Bucket Sort
+}
